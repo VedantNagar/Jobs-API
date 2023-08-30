4a64d0df6c53bac10e398c216e387f2e09445a09
Vedant <vedantnagar21@gmail.com>
2023-08-30 17:02:28 +0530

    Registration-Login

--- 04-jobs-api/starter/controllers/jobs.js
+++ 04-jobs-api/starter/controllers/jobs.js
@@ -1,11 +1,18 @@
+const Job = require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError,NotFoundError} = require('../errors')
+
 const getAllJobs = async(req,res)=>{
-    res.send('Get All Jobs')
+    const jobs = await Job.find({createdBy:req.user.userID}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs , count:jobs.length})
 }
 const getJob = async(req,res)=>{
     res.send('Get Job')
 }
 const createJob = async(req,res)=>{
-    res.json(req.user)
+    req.body.createdBy = req.user.userID;
+    const job = await Job.create(req.body);
+    res.status(StatusCodes.CREATED).json({job});
 }
 const updateJob = async(req,res)=>{
     res.send('Update Jobs')
