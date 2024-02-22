54a17c14fe2f1697fd5eb63775c9dafb07040fc3
Vedant <vedantnagar21@gmail.com>
2024-02-22 17:57:37 +0530

    Joined frontend

--- 04-jobs-api/starter/app.js
+++ 04-jobs-api/starter/app.js
@@ -3,29 +3,31 @@ require('express-async-errors');
 
 //extra security packages
 const helmet = require('helmet');
-const cors = require('cors')
-const xss = require('xss-clean')
-const rateLimit = require('express-rate-limit')
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 
 const express = require('express');
 const app = express();
 
 //connectDB
-const connectDB = require('../starter/db/connect')
-const authenticateUser = require('./middleware/authentication')
+const connectDB = require('../starter/db/connect');
+const authenticateUser = require('./middleware/authentication');
 //routers
-const authRouter = require('./routes/auth')
-const jobsRouter = require('./routes/jobs')
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy',1);
-app.use(rateLimit({
-  windowMs:15*60*1000, //15 minutes
-  max:100, //limit each IP to 100 requests per windowMs
-}));
+app.set('trust proxy', 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, //limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
@@ -33,17 +35,18 @@ app.use(xss());
 // extra packages
 
 // routes
-app.use('/api/v1/auth',authRouter)
-app.use('/api/v1/jobs',authenticateUser,jobsRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = 3000 || process.env.PORT;
+const port = 4000 || process.env.PORT;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
+    console.log('Database connected');
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
